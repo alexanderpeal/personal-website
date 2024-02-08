38658117f3d3560fdbfe9557983212a47908ea7b
Alexander Peal <aapeal04@gmail.com>
2024-02-08 16:42:21 -0500

    added section pages

---
+++ components/AboutSection.tsx
@@ -0,0 +1,10 @@
+const AboutContent = () => {
+    return (
+      <div>
+        <h2>About Us</h2>
+        <p>This is the about section.</p>
+      </div>
+    );
+  };
+  
+  export default AboutContent;
