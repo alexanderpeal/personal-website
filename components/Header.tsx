fa06e6a9c99cf000792a7b4beaaf55bfd33f31d1
alexanderpeal <44208038+alexanderpeal@users.noreply.github.com>
2024-02-09 21:06:58 -0500

    again

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,14 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ setContent }) => {
     return (
       <header className="p-4">
-        <div className="container mx-auto flex justify-center items-center text-netural-900">
+        <div className="container mx-auto flex justify-center items-center">
           <h1 className="text-3xl font-bold"></h1>
           <nav>
-            <ul className="flex space-x-4">
-              <li><a href="#" onClick={() => setContent('home')} className="hover:text-gray-300">home</a></li>
-              <li><a href="#resume" onClick={() => setContent('links')} className="hover:text-gray-300">links</a></li>
-              <li><a href="#contact" onClick={() => setContent('resume')} className="hover:text-gray-300">resume</a></li>
-              <li><a href="#contact" onClick={() => setContent('contact')} className="hover:text-gray-300">contact</a></li>
+            <ul className="flex space-x-4  text-netural-900">
+              <li><a href="#" onClick={() => setContent('home')} className="hover:text-neutral-300">home</a></li>
+              <li><a href="#resume" onClick={() => setContent('links')} className="hover:text-neutral-300">links</a></li>
+              <li><a href="#contact" onClick={() => setContent('resume')} className="hover:text-neutral-300">resume</a></li>
+              <li><a href="#contact" onClick={() => setContent('contact')} className="hover:text-neutral-300">contact</a></li>
             </ul>
           </nav>
         </div>
--- components/content/ContactContent.tsx
+++ components/content/ContactContent.tsx
@@ -1,8 +1,8 @@
 const ContactContent = () => {
     return (
-        <div className="text-netural-900">
+        <div>
         
-        <p> 
+        <p className="text-netural-900"> 
             Send me a message on <a href="https://www.linkedin.com/in/alexpeal/" className="text-blue-500 no-underline hover:underline">LinkedIn</a>!
         </p>
 
