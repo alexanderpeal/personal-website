30560757017435b94430d57b9aef5f57edf3c041
alexanderpeal <44208038+alexanderpeal@users.noreply.github.com>
2024-02-09 21:28:44 -0500

    again7

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,20 +5,21 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ setContent }) => {
     return (
       <div>
-        <header className="p-4">
-        <div className="container mx-auto flex justify-center items-center text-netural-900">
-          <nav>
-            <ul className="flex space-x-4 text-netural-900">
-              <li><a href="#" onClick={() => setContent('home')} className="hover:text-neutral-300">home</a></li>
-              <li><a href="#resume" onClick={() => setContent('links')} className="hover:text-neutral-300 text-netural-900">links</a></li>
-              <li><a href="#contact" onClick={() => setContent('resume')} className="hover:text-neutral-300 text-netural-900">resume</a></li>
-              <li><a href="#contact" onClick={() => setContent('contact')} className="hover:text-neutral-300 text-netural-900">contact</a></li>
-            </ul>
-          </nav>
+        <div className="text-neutral-900">
+          <header className="p-4">
+            <div className="container mx-auto flex justify-center items-center text-netural-900">
+              <nav>
+                <ul className="flex space-x-4 text-netural-900">
+                  <li><a href="#" onClick={() => setContent('home')} className="hover:text-neutral-300">home</a></li>
+                  <li><a href="#resume" onClick={() => setContent('links')} className="hover:text-neutral-300 text-netural-900">links</a></li>
+                  <li><a href="#contact" onClick={() => setContent('resume')} className="hover:text-neutral-300 text-netural-900">resume</a></li>
+                  <li><a href="#contact" onClick={() => setContent('contact')} className="hover:text-neutral-300 text-netural-900">contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </header>
         </div>
-      </header>
       </div>
-      
     );
   };
   
