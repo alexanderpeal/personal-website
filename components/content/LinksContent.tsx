fed05008a2bc2c210c0e7dc1e33ce6a9b2e4cce3
alexanderpeal <44208038+alexanderpeal@users.noreply.github.com>
2024-02-09 21:31:32 -0500

    everything works

--- components/content/LinksContent.tsx
+++ components/content/LinksContent.tsx
@@ -1,7 +1,7 @@
 const LinksContent = () => {
   return (
   <div>
-    <ul className="list-disc text-neutral-900">
+    <ul className="list-none text-neutral-900">
       <li><a href="https://vthacks.com/" className="text-blue-500 no-underline hover:underline">VTHacks</a></li>
       <li><a href="https://www.linkedin.com/in/alexpeal/" className="text-blue-500 no-underline hover:underline">LinkedIn</a></li>
       <li><a href="https://github.com/alexanderpeal" className="text-blue-500 no-underline hover:underline">GitHub</a></li>
