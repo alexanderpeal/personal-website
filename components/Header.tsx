320a09bc38899b772bd77763ff0d144c97d93219
alexanderpeal <44208038+alexanderpeal@users.noreply.github.com>
2024-02-09 21:15:03 -0500

    again3

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,8 +8,8 @@ const Header: React.FC<HeaderProps> = ({ setContent }) => {
         <div className="container mx-auto flex justify-center items-center">
           <h1 className="text-3xl font-bold"></h1>
           <nav>
-            <ul className="flex space-x-4">
-              <li><a href="#" onClick={() => setContent('home')} className="hover:text-neutral-300 text-netural-900">home</a></li>
+            <ul className="flex space-x-4 text-netural-900">
+              <li><a href="#" onClick={() => setContent('home')} className="hover:text-neutral-300">home</a></li>
               <li><a href="#resume" onClick={() => setContent('links')} className="hover:text-neutral-300 text-netural-900">links</a></li>
               <li><a href="#contact" onClick={() => setContent('resume')} className="hover:text-neutral-300 text-netural-900">resume</a></li>
               <li><a href="#contact" onClick={() => setContent('contact')} className="hover:text-neutral-300 text-netural-900">contact</a></li>
