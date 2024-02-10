459356d77ddb82108f363e179c6bd37dd1160b97
alexanderpeal <44208038+alexanderpeal@users.noreply.github.com>
2024-02-09 21:22:50 -0500

    again5

--- components/content/ContactContent.tsx
+++ components/content/ContactContent.tsx
@@ -1,7 +1,7 @@
 const ContactContent = () => {
     return (
         <div>
-            <div  className="text-md text-netural-900">
+            <div className="text-md text-gray-900">
                 <p> 
                     Send me a message on <a href="https://www.linkedin.com/in/alexpeal/" className="text-blue-500 no-underline hover:underline">LinkedIn</a>!
                 </p>
